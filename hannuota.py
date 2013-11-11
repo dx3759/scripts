6e7138099010dd2fcc686b0138fbd08e9e991281
dx3759 <dragonx3759@gmail.com>
2013-11-12 01:10:54 +0800

    add hannuota.py

---
+++ hannuota.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# By who 2013-11-12
+
+# TODO  hannuota.py
+
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def hanoi():
+	pass
+
+def main():
+	pass
+
+if __name__=="__main__":
+	main()
